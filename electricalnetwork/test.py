c61a8c7cc21e80ca58bc5cf253a4f1b3d034b68c
J. Hart <jw_hart@telus.net>
2014-07-19 22:30:14 -0700

    updated test.py

--- electricalnetwork/test.py
+++ electricalnetwork/test.py
@@ -1,3 +1,6 @@
+"""
+simple test / example of the electricalnetwork library
+"""
 
 from electrical import SwitchableDevice
 from electrical import ElectricalNetwork
@@ -17,11 +20,43 @@ cond2 = ElectricalConductor(distribution,'ABC', fuse2, simpleNode)
 cond3 = ElectricalConductor(distribution,'ABC', simpleNode, fuse3)
 cond4 = ElectricalConductor(distribution,'ABC', fuse3, testFeeder2)
 
+###  TESTFEEDER -- (cond1)-- SIMPLENODE -- (cond2) -- FUSE2
+###`                             |
+###                           (cond3)
+###                              |
+###                            FUSE3 -- (cond4) -- TESTFEEDER2
 
-print "Starting"
+
+print "FUSE 3 source will all closed"
 srcs = fuse3.getSources()
 for src in srcs:
     print "   {0}".format(src.designation)
+
+print "Simple Node sources "
+srcs = simpleNode.getSources()
+for src in srcs:
+    print "   {0}".format(src.designation)
+
+
+print "Fuse 3 sources after opening fuse 3"
+fuse3.open(phase='all')
+srcs = fuse3.getSources()
+for src in srcs:
+    print "   {0}".format(src.designation)
+
+print "Simple Node sources "
+srcs = simpleNode.getSources()
+for src in srcs:
+    print "   {0}".format(src.designation)
+
+print "Closing"
+fuse3.close(phase='all')
+srcs = fuse3.getSources()
+for src in srcs:
+    print "   {0}".format(src.designation)
+##fuse.open('B')
+
+#print fuse
 
 
 print "Opening"
